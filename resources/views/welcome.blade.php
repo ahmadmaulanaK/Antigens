396ab5fcbd1af55f382f7125a08e5d99af47ecc2
ahmadmaulanaK <78901520+ahmadmaulanaK@users.noreply.github.com>
2022-02-16 16:26:29 +0700

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,51 +1,45 @@
 @extends('layouts.auth')
 
 @section('title')
-<title>Welcome </title>
+<title>Login</title>
 @endsection
 
 @section('content')
-<div class="container">
-    <ui-view class="app ng-scope">
-        <!-- uiView: -->
-        <ui-view class="ng-scope">
-            <div class="app flex-row align-items-center ng-scope">
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-md-8">
-                            <div class="card-group">
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card-group">
+            <div class="card p-4">
+                <div class="card-body">
 
-                                <div class="card text-white bg-primary py-5 d-md-down-none" style="width:44%">
-                                    <div class="card-body text-center">
-                                        <div>
-                                            <h2>Selamat Datang</h2>
-                                            <p class="fst-italic"> Resep kesuksesan adalah bekerja keras dan pantang menyerah.</p>
-                                           
 
-                                            @if (Route::has('login'))
-                                          
-                                                @auth
-                                                <a href="{{ url('/home') }}"
-                                                    class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-                                                @else
-                                                <a href="{{ route('login') }}"
-                                                    class="btn btn-success active "><i
-                                                        class=""> LOGIN</i></a>
+                    <!-- ACTIONNYA MENGARAH PADA URL /LOGIN -->
+                    <!-- UNTUK MENCARI TAU TUJUAN URI DARI ROUTE NAME DIBAWAH, PADA COMMAND LINE, KETIKKAN PHP ARTISAN ROUTE:LIST DAN CARI URI YANG MENGGUNAKAN METHOD POST -->
+                    <!-- KARENA URI /LOGIN DENGAN METHOD GET DIGUNAKAN UNTUK ME-LOAD VIEW HALAMAN LOGIN -->
+                    <!-- PENGGUNAAN ROUTE() APABILA ROUTING TERSEBUT MEMILIKI NAMA, ADAPUN NAMENYA ADA PADA COLOM NAME ROUTE:LIST -->
+                    <!-- JIKA ROUTINGNYA TIDAK MEMILIKI NAMA, MAKA GUNAKAN HELPER URL() DAN DIDALAMNYA ADALAH URINYA. CONTOH URL('/LOGIN') -->
+                    <h2>Selamat Datang</h2>
+                    <p class="fst-italic"> Resep kesuksesan adalah bekerja keras dan pantang menyerah.</p>
 
-                                             
-                                                @endauth
-                                          
-                                            @endif
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+
+                    @if (Route::has('login'))
+
+                    @auth
+                    <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                    @else
+                    <a href="{{ route('login') }}" class="btn btn-success active "><i class=""> LOGIN</i></a>
+
+
+                    @endauth
+
+                    @endif
                 </div>
             </div>
-        </ui-view>
-    </ui-view>
-</div>
+            <div class="card text-white bg-primary py-5 d-md-down-none" style="width:44%">
+                <div class="card-body text-center">
+
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
