edbb32b143bda08420b007ede301a83a473c4e50
ahmadmaulanaK <78901520+ahmadmaulanaK@users.noreply.github.com>
2022-02-23 17:09:17 +0700

    form register

--- resources/views/Registers/register.blade.php
+++ resources/views/Registers/register.blade.php
@@ -8,6 +8,7 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/@coreui/coreui@4.1.0/dist/css/coreui.min.css" rel="stylesheet" integrity="sha384-J2WCKfhPxbtZAT+USOoPNCnZVozpAaGtaH/LUcSKXjdCXN85i5fGMTy1Agp8HChK" crossorigin="anonymous">
 </head>
 
 <body class="bg-light">
@@ -30,24 +31,29 @@
             <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
 
+            <br>
+            
             <label for="basic-url ">Nomor Indentitas</label>
             <div class="input-group mb-3 ">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">NIK</span>
+                </div>
                
-                <input type="text" class="form-control" placeholder="NIK" name="NIK"
+                <input type="text" class="form-control" placeholder="" name="NIK"
                     value="{{ old('NIK') }}">
                 <br>
                 <p class="text-danger">{{ $errors->first('NIK') }}</p>
             </div>
             <label for="basic-url">Nama Lengkap</label>
             <div class="input-group mb-3">
-               
-                <input type="text" class="form-control" placeholder="......." name="name" value="{{ old('Name') }}">
+              
+                <input type="text" class="form-control" placeholder="" name="name" value="{{ old('Name') }}">
                 <br> <p class="text-danger">{{ $errors->first('name') }}</p>
             </div>
             <label for="basic-url">Email Aktip</label>
             <div class="input-group mb-3">
                
-                <input type="text" class="form-control" placeholder="email" name="email" value="{{ old('email') }}">
+                <input type="text" class="form-control" placeholder="" name="email" value="{{ old('email') }}">
                 <br> <p class="text-danger">{{ $errors->first('email') }}</p>
             </div>
             <label for="basic-url">Nomor Telphone</label>
@@ -63,7 +69,7 @@
                     <input style="" type="date" id="start" name="TTL" value="{{ old('TTL') }}" min="1950-01-01">
                     <p class="text-danger">{{ $errors->first('TTL') }}</p>
                    </div>
-                   <label for="basic-url">Tanggal Lahir</label>
+                   <label for="basic-url">Jenis Kelamin</label>
                    <div class="col">
                       <select class="custom-select" id="inputGroupSelect01">
                         <option value="">Pilih....</option>
@@ -83,35 +89,86 @@
 
               
             </div>
-            <label for="basic-url">Nomor Indentitas</label>
+         
+            <label for="basic-url">Alamat Lengkap</label>
             <div class="input-group mb-3">
                
-              
+                <textarea class="form-control" name="address" aria-label="With textarea"
+                            value="{{ old('address') }}">{{ old('address') }}</textarea>
+                        <p class="text-danger">{{ $errors->first('address') }}</p>
             </div>
-            <label for="basic-url">Nomor Indentitas</label>
             <div class="input-group mb-3">
-               
-                <input type="text" class="form-control" placeholder="Nomor Identitas/NIK" name="NIK"
-                    value="{{ old('NIK') }}">
-                <br>
-                <p class="text-danger">{{ $errors->first('NIK') }}</p>
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Jenis Tes</span>
+                </div>
+                <select name="category_id" class="custom-select">
+                  <option value="">Pilih ....
+                  </option>
+                  @foreach ($category as $row)
+                  <option value="{{ $row->id }}"
+                      {{ old('category_id') == $row->id ? 'selected':'' }}>{{ $row->name }}
+                  </option>
+                  @endforeach
+              </select>
+              <p class="text-danger">{{ $errors->first('category_id') }}</p>
             </div>
-            <label for="basic-url">Nomor Indentitas</label>
             <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Pilih Lokasi</span>
+                </div>
                
-                <input type="text" class="form-control" placeholder="Nomor Identitas/NIK" name="NIK"
-                    value="{{ old('NIK') }}">
-                <br>
-                <p class="text-danger">{{ $errors->first('NIK') }}</p>
+                <select name="titik_id" class="custom-select">
+                    <option value="">pilih Lokasi....
+                    </option>
+                    @foreach ($titik as $row)
+                    <option value="{{ $row->id }}"
+                        {{ old('titik_id') == $row->id ? 'selected':'' }}>{{ $row->name }}
+                    </option>
+                    @endforeach
+                </select>
+                <p class="text-danger">{{ $errors->first('titik_id') }}</p>
             </div>
-            <label for="basic-url">Nomor Indentitas</label>
-            <div class="input-group mb-3">
+
+            <div class="input-group mb-3 mt-5">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" required>
+                    <label class="form-check-label" for="flexCheckDefault" >
+                        Dengan ini menyatakan data yang saya isi adalah sebenar-benarnya, dan saya bersedia
+                        untuk diambil spesimen untuk pemeriksaan Covid-19.
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" required>
+                    <label class="form-check-label " for="flexCheckChecked">
+                        Apabila hasil akan dimasukan ke dalam database pemerintah lewat program all record
+                        kementrian kesehatan
+                    </label>
+                </div>
+
+                <div class="card mt-3">
+                    <div class="card-body" style='text-align:justify;font-size:10px;'>
+                        Untuk hasil tes PCR & Tes Antigen Home Service akan dikirimkan melalui Whatsapp atau
+                        email yang tertera didalam informed consent ini, Hasil pemeriksaan hanya berlaku
+                        pada saat pemeriksaan dilakukan
+                    </div>
+                </div>
+                
+            </div>
+            <button type="submit" class="btn btn-block btn-success float-right">Register</button>
+        </form>
+            <div class="mt-5 container">
+                <div class="row justify-content-center">
+                       
+                    <div class="col ">
+                        <img src="{{ asset('assets/img/branch/bandung.png') }}" width="150" height="">
+                    </div>
+                    <div class="col ">
+                        <img src="{{ asset('assets/img/logofix.png') }}" width="180" height="">
+                    </div>
+                </div>
                
-                <input type="text" class="form-control" placeholder="Nomor Identitas/NIK" name="NIK"
-                    value="{{ old('NIK') }}">
-                <br>
-                <p class="text-danger">{{ $errors->first('NIK') }}</p>
             </div>
+          
           
         </div>
     </div>
